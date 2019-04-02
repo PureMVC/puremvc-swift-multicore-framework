efadc59746a5ae450dc465f5650341512fa45204
saadshams <saadshams@hotmail.com>
2019-04-02 10:47:34 -0400

    SPM update

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "puremvc-swift-multicore-framework",
-            targets: ["puremvc-swift-multicore-framework"]),
+            name: "PureMVC",
+            targets: ["PureMVC"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "puremvc-swift-multicore-framework",
+            name: "PureMVC",
             dependencies: []),
         .testTarget(
-            name: "puremvc-swift-multicore-frameworkTests",
-            dependencies: ["puremvc-swift-multicore-framework"]),
+            name: "PureMVCTests",
+            dependencies: ["PureMVC"]),
     ]
 )
