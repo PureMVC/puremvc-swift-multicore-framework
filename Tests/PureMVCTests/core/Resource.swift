477fc0f6770a318fefedbcb812066eb1b9aa5f16
Saad <saadmuizz@gmail.com>
2025-05-31 18:14:50 -0400

    update
    license

--- Tests/PureMVCTests/core/Resource.swift
+++ Tests/PureMVCTests/core/Resource.swift
@@ -3,7 +3,7 @@
 //  PureMVC SWIFT Multicore
 //
 //  Copyright(c) 2020 Saad Shams <saad.shams@puremvc.org>
-//  Your reuse is governed by the Creative Commons Attribution 3.0 License
+//  Your reuse is governed by the BSD 3-Clause License
 //
 
 public class Resource {
